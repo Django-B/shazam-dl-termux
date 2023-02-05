0af37a309d8c1d65edde343a6732076ed9339c26
django <django.ppp@gmail.com>
2023-02-05 16:16:19 +0300

    Bugs fixed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import sys
 
+
+headers = {'Accept': 'application/json'}
+
 def get_id(url: str):
     id = url.partition('track/')[2]\
             .partition('/')[0]
@@ -10,18 +13,12 @@ def get_yt_link(track_id: str):
     template_url = "https://www.shazam.com/discovery/v5/ru/RU/web/-/track/{}?shazamapiversion=v3&video=v3"
     url = template_url.format(track_id)
 
-    res = requests.get(url)
+    res = requests.get(url, headers=headers)
     data = res.json()
     json_url = data['sections'][1]['youtubeurl'] if 'youtubeurl' in data['sections'][1]\
             else data['sections'][2]['youtubeurl']
-    '''
-    if 'youtubeurl' in data['sections'][1]:
-        json_url = data['sections'][1]['youtubeurl'] 
-    else:
-        json_url = data['sections'][2]['youtubeurl'] 
-    '''
     
-    res = requests.get(json_url)
+    res = requests.get(json_url, headers=headers)
     yt_link = res.json()['actions'][0]['uri']
 
     return yt_link
